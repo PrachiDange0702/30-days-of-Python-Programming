1430e0847cba42cd8a6456b491cdb3dfddaad31c
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-08-28 19:44:25 +0530

    Create IfElifstatement.py

---
+++ Day 1/IfElifstatement.py
@@ -0,0 +1,17 @@
+# Demostrating the use of Python If Elif statement
+x=int(input("enter day number:"))
+
+if(x==1):
+    print("monday")
+elif(x==2):
+    print("tuesday")
+elif(x==3):
+    print("wdnesday")
+elif(x==4):
+    print("thursday")
+elif(x==5):
+    print("friday")
+elif(x==6):
+    print("saturday")
+else:
+    print("Sunday")
