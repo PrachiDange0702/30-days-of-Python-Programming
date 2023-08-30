e2b30f307694af13ba42c141f31ba3547123087f
Prachi Dange <114732142+PrachiDange0702@users.noreply.github.com>
2023-08-30 20:03:25 +0530

    Create Primerange.py

---
+++ Day 3/Primerange.py
@@ -0,0 +1,12 @@
+# Program to print first N prime numbers
+def isPrime(n):
+ if(n==1 or n==0):
+  return False
+ for i in range(2,n):
+  if(n%i==0):
+   return False
+ return True
+N = int(input("Enter range:"))
+for i in range(1,N+1):
+ if(isPrime(i)):
+  print(i,end=" "
